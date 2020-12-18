f69a6f3ea2d1cb7ab2918b24f8cc8d860b3d0b61
刘彬 <1129434577@qq.com>
2020-12-18 10:52:37 +0800

    优化dome

--- LBPhotoPreviewControllerExample/TestViewController.m
+++ LBPhotoPreviewControllerExample/TestViewController.m
@@ -8,22 +8,49 @@
 
 #import "TestViewController.h"
 #import "LBPhotoPreviewController.h"
-
+#import "UIButton+WebCache.h"
 
 @interface TestViewController ()
+@property (nonatomic, strong) NSMutableArray<UIButton *> *btnArray;
 @end
 
 @implementation TestViewController
-
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _btnArray = [NSMutableArray array];
+    }
+    return self;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake((CGRectGetWidth(self.view.frame)-300)/2, 150, 300, 400)];
-    btn.backgroundColor = [UIColor lightGrayColor];
-    btn.layer.cornerRadius = 10;
-    [btn setTitle:@"本地和网络图片流量" forState:UIControlStateNormal];
-    [btn addTarget:self action:@selector(phonePreviewTest:) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:btn];
+    self.title = @"点击开始浏览";
+    
+    NSArray<NSString *> *imageNameArray = @[@"1.jpg",
+                                            @"2.png",
+                                            @"https://image-static.segmentfault.com/419/538/4195380633-58e8f762b0d73_articlex",
+                                            @"3.jpg"];
+    
+    
+    [imageNameArray enumerateObjectsUsingBlock:^(NSString * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake((CGRectGetWidth(self.view.frame)-300)/2, 100+idx*(25+200), 300, 200)];
+        btn.layer.cornerRadius = 10;
+        [btn addTarget:self action:@selector(phonePreviewTest:) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:btn];
+        
+        
+        UIImage *image = [UIImage imageNamed:obj];
+        if (image) {
+            [btn setImage:image forState:UIControlStateNormal];
+        }else{
+            [btn sd_setImageWithURL:[NSURL URLWithString:obj] forState:UIControlStateNormal];
+        }
+        [self.btnArray addObject:btn];
+    }];
+    
+    
 }
 
 -(void)phonePreviewTest:(UIButton *)sender{
@@ -32,9 +59,9 @@
     LBImageObject *urlImag = [LBImageObject objectWithImageUrl:[NSURL URLWithString:@"https://image-static.segmentfault.com/419/538/4195380633-58e8f762b0d73_articlex"]];
     LBImageObject *imag3 = [LBImageObject objectWithImage:[UIImage imageNamed:@"3.jpg"]];
     
-    LBPhotoPreviewController *photoPreviewC = [[LBPhotoPreviewController alloc] initWithSourceViews:@[sender]];
+    LBPhotoPreviewController *photoPreviewC = [[LBPhotoPreviewController alloc] initWithSourceViews:self.btnArray];
     photoPreviewC.imageObjectArray = @[imag1,imag2,urlImag,imag3];
-    photoPreviewC.previewScrollView.currentPage = 1;
+    photoPreviewC.previewScrollView.currentPage = [self.btnArray indexOfObject:sender];
     [self presentViewController:photoPreviewC animated:YES completion:nil];
 }
 @end
